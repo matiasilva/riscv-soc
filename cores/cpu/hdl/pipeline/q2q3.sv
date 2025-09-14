5209c29dd5691d4f315ed0d61d6adeb5c3760676
Matias Silva <git@matiasilva.com>
2025-09-14 17:33:55 +0100

    Propagate cpu_types.vh to pipeline registers

--- cores/cpu/hdl/pipeline/q2q3.sv
+++ cores/cpu/hdl/pipeline/q2q3.sv
@@ -28,67 +28,34 @@
 //   Pipeline register between Q2 (instruction decode/register file access) and Q3 (execute/ALU)
 //
 // Parameters:
-//   CTRL_WIDTH - Width of control signal bus
+//   None
 
-module q2q3 #(
-    parameter CTRL_WIDTH = 16
-) (
-    input                   i_clk,
-    input                   i_rst_n,
-    input  [          31:0] i_pc,
-    output [          31:0] o_pc,
-    input  [          31:0] i_reg_rd_data1,
-    output [          31:0] o_reg_rd_data1,
-    input  [          31:0] i_reg_rd_data2,
-    output [          31:0] o_reg_rd_data2,
-    input  [           4:0] i_reg_wr_port,
-    output [           4:0] o_reg_wr_port,
-    input  [CTRL_WIDTH-1:0] i_ctrl_q2,
-    output [CTRL_WIDTH-1:0] o_ctrl_q2,
-    input  [          31:0] i_instr,
-    output [          31:0] o_instr,
-    input  [          31:0] i_pc_incr,
-    output [          31:0] o_pc_incr
+`include "cpu_types.vh"
 
+module q2q3 (
+    input i_clk,
+    input i_rst_n,
+    input q2q3_t i_q2q3,
+    output q2q3_t o_q2q3
 );
 
-  reg [          31:0] next_pc;
-  reg [          31:0] next_pc_incr;
-
-  reg [          31:0] next_reg_rd_data1;
-  reg [          31:0] next_reg_rd_data2;
-  reg [           4:0] next_reg_wr_port;
-  reg [CTRL_WIDTH-1:0] next_ctrl_q2;
-  reg [          31:0] next_instr;
+  q2q3_t next_q2q3;
 
   always @(posedge i_clk or negedge i_rst_n) begin
     if (~i_rst_n) begin
-      next_pc           <= 0;
-      next_reg_rd_data1 <= 0;
-      next_reg_rd_data2 <= 0;
-      next_reg_wr_port  <= 0;
-      next_ctrl_q2      <= 0;
-      next_instr        <= 32'h00000013;  //  NOP;
-      next_pc_incr      <= 0;
-
+      next_q2q3 <= '{
+        pc: '0,
+        pc_incr: '0,
+        reg_rd_data1: '0,
+        reg_rd_data2: '0,
+        reg_wr_port: '0,
+        ctrl: '0,
+        insn: 32'h00000013  // NOP
+      };
     end else begin
-      next_pc           <= i_pc;
-      next_reg_rd_data1 <= i_reg_rd_data1;
-      next_reg_rd_data2 <= i_reg_rd_data2;
-      next_reg_wr_port  <= i_reg_wr_port;
-      next_ctrl_q2      <= i_ctrl_q2;
-      next_instr        <= i_instr;
-      next_pc_incr      <= i_pc_incr;
-
+      next_q2q3 <= i_q2q3;
     end
   end
 
-  assign o_pc           = next_pc;
-  assign o_pc_incr      = next_pc_incr;
-
-  assign o_reg_rd_data1 = next_reg_rd_data1;
-  assign o_reg_rd_data2 = next_reg_rd_data2;
-  assign o_reg_wr_port  = next_reg_wr_port;
-  assign o_ctrl_q2      = next_ctrl_q2;
-  assign o_instr        = next_instr;
+  assign o_q2q3 = next_q2q3;
 endmodule
--- cores/cpu/hdl/pipeline/q4q5.sv
+++ cores/cpu/hdl/pipeline/q4q5.sv
@@ -28,51 +28,33 @@
 //   Pipeline register between Q4 (memory access) and Q5 (write-back)
 //
 // Parameters:
-//   CTRL_WIDTH - Width of control signal bus
+//   None
 
-module q4q5 #(
-    parameter CTRL_WIDTH = 16
-) (
-    input                   i_clk,
-    input                   i_rst_n,
-    input  [          31:0] i_alu_out,
-    output [          31:0] o_alu_out,
-    input  [          31:0] i_mem_rdata,
-    output [          31:0] o_mem_rdata,
-    input  [           4:0] i_reg_wr_port,
-    output [           4:0] o_reg_wr_port,
-    input  [CTRL_WIDTH-1:0] i_ctrl_q4,
-    output [CTRL_WIDTH-1:0] o_ctrl_q4,
-    input  [          31:0] i_instr,
-    output [          31:0] o_instr
+`include "cpu_types.vh"
+
+module q4q5 (
+    input i_clk,
+    input i_rst_n,
+    input q4q5_t i_q4q5,
+    output q4q5_t o_q4q5
 );
 
-  reg [          31:0] next_reg_wr_port;
-  reg [          31:0] next_alu_out;
-  reg [          31:0] next_mem_rdata;
-  reg [CTRL_WIDTH-1:0] next_ctrl_q4;
-  reg [          31:0] next_instr;
+  q4q5_t next_q4q5;
 
   always @(posedge i_clk or negedge i_rst_n) begin
     if (~i_rst_n) begin
-      next_alu_out     <= 0;
-      next_mem_rdata   <= 0;
-      next_reg_wr_port <= 0;
-      next_ctrl_q4     <= 0;
-      next_instr       <= 32'h00000013;  //  NOP;
+      next_q4q5 <= '{
+        alu_out: '0,
+        mem_rdata: '0,
+        reg_wr_port: '0,
+        ctrl: '0,
+        insn: 32'h00000013  // NOP
+      };
     end else begin
-      next_alu_out     <= i_alu_out;
-      next_mem_rdata   <= i_mem_rdata;
-      next_reg_wr_port <= i_reg_wr_port;
-      next_ctrl_q4     <= i_ctrl_q4;
-      next_instr       <= i_instr;
+      next_q4q5 <= i_q4q5;
     end
   end
 
-  assign o_alu_out     = next_alu_out;
-  assign o_mem_rdata   = next_mem_rdata;
-  assign o_reg_wr_port = next_reg_wr_port;
-  assign o_ctrl_q4     = next_ctrl_q4;
-  assign o_instr       = next_instr;
+  assign o_q4q5 = next_q4q5;
 
 endmodule
