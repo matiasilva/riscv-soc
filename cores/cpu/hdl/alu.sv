53456e62ff23ab2c65a84e2c1882f4caf511e21a
Matias Silva <git@matiasilva.com>
2025-09-14 17:30:14 +0100

    Add ALU exception and refactor ALU

--- cores/cpu/hdl/alu.sv
+++ cores/cpu/hdl/alu.sv
@@ -30,25 +30,25 @@
 //
 // Control Signals:
 //   4'b0000 ADD  - Addition
-//   4'b1000 SUB  - Subtraction
+//   4'b0001 SLL  - Shift Left Logical
 //   4'b0010 SLT  - Set Less Than (signed)
 //   4'b0011 SLTU - Set Less Than Unsigned
-//   4'b0111 AND  - Bitwise AND
-//   4'b0110 OR   - Bitwise OR
 //   4'b0100 XOR  - Bitwise XOR
-//   4'b0001 SLL  - Shift Left Logical
 //   4'b0101 SRL  - Shift Right Logical
+//   4'b0110 OR   - Bitwise OR
+//   4'b0111 AND  - Bitwise AND
+//   4'b1000 SUB  - Subtraction
 //   4'b1101 SRA  - Shift Right Arithmetic
 
 `include "cpu_types.vh"
+
 module alu (
-    input  logic [31:0] i_alu_a,
-    input  logic [31:0] i_alu_b,
-    input  logic [ 3:0] i_aluctrl_ctrl,
-    output logic [31:0] o_alu_out
+    input logic [31:0] i_alu_a,
+    input logic [31:0] i_alu_b,
+    input alu_op_t i_alu_ctrl,
+    output logic [31:0] o_alu_out,
+    output logic o_alu_exception
 );
-
-
 
   logic [31:0] diff;
   logic [ 4:0] shamt;
@@ -57,9 +57,8 @@ module alu (
   assign diff  = i_alu_a - i_alu_b;
   assign shamt = i_alu_b[4:0];
 
-  always_comb begin
-    result = '0;
-    unique case (aluop_t'(i_aluctrl_ctrl))
+  always_comb begin : alu_result
+    unique case (i_alu_ctrl)
       OP_ADD:  result = i_alu_a + i_alu_b;
       OP_SUB:  result = diff;
       OP_SLT: begin
@@ -78,6 +77,14 @@ module alu (
       OP_SRA:  result = $signed(i_alu_a) >>> shamt;
       default: result = '0;
     endcase
+  end
+
+  always_comb begin : alu_controller
+    if (i_alu_ctrl == OP_INVALID) begin
+      o_alu_exception = '1;
+    end else begin
+      o_alu_exception = '0;
+    end
   end
 
   assign o_alu_out = result;
